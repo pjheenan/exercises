9db7a984f851d49ab2f461b29e9700219f672833
Phillip Heenan <Phillip.Heenan@viagogo.com>
2015-01-14 19:03:01 +0000

    empty test case

---
+++ src/test/java/com/example/exercises/fizz/FizzBuzTest.java
@@ -0,0 +1,25 @@
+package com.example.exercises.fizz;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by phillip.heenan on 14/01/15.
+ */
+public class FizzBuzTest extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @Test
+    public void testStart() {
+        fail();
+    }
+}
