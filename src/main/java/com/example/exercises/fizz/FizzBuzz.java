1da1a8ef52fc77433da438fef36c175a1f8ea820
Phillip Heenan <Phillip.Heenan@viagogo.com>
2015-01-22 15:21:45 +0000

    make mutliplier and output value dynamic

--- src/main/java/com/example/exercises/fizz/FizzBuzz.java
+++ src/main/java/com/example/exercises/fizz/FizzBuzz.java
@@ -1,5 +1,9 @@
 package com.example.exercises.fizz;
 
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.Map;
+
 /**
  * Write a program that prints the numbers from 1 to 100. But for multiples of three getOutput "Fizz"
  * instead of the number and for the multiples of five getOutput "Buzz". For numbers which are multiples of both
@@ -9,19 +13,24 @@ package com.example.exercises.fizz;
  */
 public class FizzBuzz {
 
-    public String[] runProgram(int max) {
+    public String[] runProgram(int max, Map<Integer, String> values) {
         String[] result = new String[max];
         for(int i = 0; i < max; i++){
-            result[i] = String.valueOf(getOutput(i+1));
-
+            result[i] = String.valueOf(getOutput(i+1, values));
         }
         return result;
     }
 
-    protected Object getOutput(int number){
-        String builder = new StringBuilder(isMultiple(3, number)? "Fizz": "")
-                .append(isMultiple(5, number)? "Buzz" : "").toString();
-        return (builder.length()!= 0) ? builder : number;
+    protected Object getOutput(int number, Map<Integer, String> values){
+        StringBuilder builder = new StringBuilder();
+        for(Integer multiplier : values.keySet()){
+            builder.append(isMultiple(multiplier, number)? values.get(multiplier): "");
+        }
+        return (builder.length()!= 0) ? builder.toString() : number;
+
+
+//        String builder = new StringBuilder(isMultiple(3, number)? "Fizz": "")
+//                .append(isMultiple(5, number)? "Buzz" : "").toString();
     }
 
     protected boolean isMultiple(int multiplier, int input){
--- src/test/java/com/example/exercises/fizz/FizzBuzTest.java
+++ src/test/java/com/example/exercises/fizz/FizzBuzTest.java
@@ -3,14 +3,20 @@ package com.example.exercises.fizz;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by phillip.heenan on 14/01/15.
  */
 public class FizzBuzTest extends TestCase {
 
+    Map<Integer, String> map;
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        map = getIntegerStringMap();
     }
 
     @Override
@@ -25,41 +31,49 @@ public class FizzBuzTest extends TestCase {
     }
 
     @Test
-    public void tesNotMultiple() {
+    public void testNotMultiple() {
         FizzBuzz f = new FizzBuzz();
         assertFalse(f.isMultiple(3, 5));
     }
 
-    public void testExceptionThrown(){
+    public void testExceptionThrown() {
         FizzBuzz f = new FizzBuzz();
         assertFalse(f.isMultiple(0, 3));
     }
 
     public void testPrinterNotMultiple() {
         FizzBuzz f = new FizzBuzz();
-        assertEquals(1, f.getOutput(1));
+        assertEquals(1, f.getOutput(1, map));
     }
 
     public void testPrinterMultiple3() throws Exception {
         FizzBuzz f = new FizzBuzz();
-        assertEquals("Fizz", f.getOutput(3));
+        assertEquals("Fizz", f.getOutput(3, map));
     }
 
     public void testPrinterMultiple5() throws Exception {
         FizzBuzz f = new FizzBuzz();
-        assertEquals("Buzz", f.getOutput(5));
+        assertEquals("Buzz", f.getOutput(5, map));
     }
 
     public void testPrinterMultiple3And5() throws Exception {
         FizzBuzz f = new FizzBuzz();
-        assertEquals("FizzBuzz", f.getOutput(15));
+        assertEquals("FizzBuzz", f.getOutput(15, map));
     }
 
-    public void testRunProgram100() throws  Exception {
+    public void testRunProgram100() throws Exception {
         FizzBuzz f = new FizzBuzz();
-        String[] result = f.runProgram(100);
+        String[] result = f.runProgram(100, map);
         assertEquals(100, result.length);
         assertEquals("1", result[0]);
         assertEquals("Buzz", result[99]);
     }
+
+    private Map<Integer, String> getIntegerStringMap() {
+        map = new HashMap<Integer, String>();
+        map.put(3, "Fizz");
+        map.put(5, "Buzz");
+        return map;
+    }
+
 }
