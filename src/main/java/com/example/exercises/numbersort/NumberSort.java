3fb3dd6b26147df8bef58597de46e7a1f511f880
Phillip Heenan <Phillip.Heenan@viagogo.com>
2015-01-26 08:04:37 +0000

    adding new kata - number sort

---
+++ src/main/java/com/example/exercises/numbersort/NumberSort.java
@@ -0,0 +1,17 @@
+package com.example.exercises.numbersort;
+
+/**
+ * In the Pragmatic Lottery (motto: There’s One Born Every Minute, and it Might Just Be You!), we select each week’s
+ * winning combination by drawing balls. There are sixty balls, numbered (not surprisingly, as we are programmers) 0 to
+ * 59. The balls are drawn by the personable, but somewhat distracted, Daisy Mae. As a result, some weeks five numbers
+ * are drawn, while other weeks seven, nine, or even fifteen balls make it to the winner’s rack. Regardless of the number
+ * of balls drawn, our viewers need to see the list of winning numbers in sorted order just as soon as possible. So, your
+ * challenge is to come up with some code that accepts each number as it is drawn and presents the sorted list of numbers
+ * so far.
+ *
+ * Created by phillip.heenan on 26/01/15.
+ */
+public class NumberSort {
+
+
+}
---
+++ src/test/java/com/example/exercises/numbersort/NumberSortTest.java
@@ -0,0 +1,25 @@
+package com.example.exercises.numbersort;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @GIVEN   a new lottery drawer
+ * @WHEN    a number is drawn from the bucket and added to the results
+ * @THEN    the results should be returned sorted in ascending order
+ *
+ * Created by phillip.heenan on 26/01/15.
+ */
+public class NumberSortTest extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+}
