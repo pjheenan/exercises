baf422a3049828dfe07849a41146cf46fe609528
Phillip Heenan <Phillip.Heenan@viagogo.com>
2015-02-17 14:50:39 +0000

    finish kata identifying performance timings etc

--- src/main/java/com/example/exercises/anagram/Anagram.java
+++ src/main/java/com/example/exercises/anagram/Anagram.java
@@ -4,21 +4,48 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by phillip.heenan on 02/02/15.
  */
 public class Anagram {
 
+    List<String> dictionary;
+    Set<Character> letters;
+
+    public Anagram() {
+        dictionary = getDictionary();
+    }
+
+    //TODO: beat performance: 156 - 164 ms, clean (248)
+
     public List<String> find(String word) {
-        List<String> dictionary = getDictionary();
-
         List<String> result = new ArrayList<String>();
+        Map<Character, Integer> counts = getCounts(word, false);
+        letters = counts.keySet();
+        for(String value: dictionary){
+            if(counts.equals(getCounts(value, true)) && !word.equals(value)){
+                result.add(value);
+            }
+        }
+        return result;
+    }
 
+    public Map<Character, Integer> getCounts (String word, boolean hasLetters){
+        Map<Character, Integer> result = new HashMap<Character, Integer>();
+        if (word == null) {
+            return result;
+        }
+        word = word.toLowerCase();
+        for (char character : word.toCharArray()) {
+            if (isAlphaCharacter(character)) {
+                if (hasLetters && !letters.contains(character)) {
+                    return null;
+                }
+                result.put(character, result.containsKey(character) ? result.get(character) + 1: 1);
+            }
+        }
         return result;
     }
 
@@ -38,6 +65,10 @@ public class Anagram {
             e.printStackTrace();
         }
         return words;
+    }
+
+    protected boolean isAlphaCharacter(char c) {
+        return ('a' <= c && c <= 'z');
     }
 
 }
--- src/test/java/com/example/exercises/anagram/AnagramTest.java
+++ src/test/java/com/example/exercises/anagram/AnagramTest.java
@@ -2,6 +2,10 @@ package com.example.exercises.anagram;
 
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by phillip.heenan on 02/02/15.
  */
@@ -15,5 +19,100 @@ public class AnagramTest extends TestCase {
         anagram = new Anagram();
 
     }
+
+    public void testDictionaryAnagram() {
+        String input = "crepitus";
+        Long start = System.currentTimeMillis();
+        List<String> result = anagram.find(input);
+        System.out.println("time: " + (System.currentTimeMillis() - start));
+        assertEquals(5, result.size());
+    }
+
+    public void testCounter() throws Exception {
+        Map<Character, Integer> counts = anagram.getCounts("caar", false);
+        assertNotNull(counts);
+        assertEquals(3, counts.size());
+        assertEquals(1, (int) counts.get('c'));
+        assertEquals(2, (int) counts.get('a'));
+        assertEquals(1, (int) counts.get('r'));
+    }
+
+    public void testNullWord() throws Exception {
+        Map<Character, Integer> counts = anagram.getCounts(null, false);
+        assertNotNull(counts);
+        assertEquals(0, counts.size());
+    }
+
+    public void testEmptyWord() throws Exception {
+        Map<Character, Integer> counts = anagram.getCounts("", false);
+        assertNotNull(counts);
+        assertEquals(0, counts.size());
+    }
+
+    public void testFindZeroResults() throws Exception {
+        List<String> result = anagram.find("");
+        assertNotNull(result);
+        assertEquals(0, result.size());
+    }
+
+    public void testFind() throws Exception{
+        List<String> fakeDictionary = new ArrayList<String>();
+        fakeDictionary.add("Ark");
+        fakeDictionary.add("Spanner");
+        fakeDictionary.add("Gym");
+        fakeDictionary.add("Auk");
+        anagram.dictionary = fakeDictionary;
+
+        List<String> result = anagram.find("Ark");
+        assertNotNull(result);
+        assertEquals(0, result.size());
+        assertFalse(result.contains("Ark"));
+    }
+
+    public void testAnagram() {
+        List<String> fakeDictionary = new ArrayList<String>();
+        fakeDictionary.add("Rak");
+        anagram.dictionary = fakeDictionary;
+
+        List<String> result = anagram.find("Ark");
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals("Rak", result.get(0));
+    }
+
+    public void testAnagramNonAlpha() {
+        List<String> fakeDictionary = new ArrayList<String>();
+        fakeDictionary.add("Arizonian's");
+        fakeDictionary.add("Arizonians");
+        fakeDictionary.add("Spanner");
+        fakeDictionary.add("Sazironian");
+        fakeDictionary.add("Gym");
+        fakeDictionary.add("Auk");
+        anagram.dictionary = fakeDictionary;
+
+        List<String> result = anagram.find("Sazironian");
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertTrue(result.contains("Arizonian's"));
+    }
+
+    public void testAnagramAccented() {
+        List<String> fakeDictionary = new ArrayList<String>();
+        fakeDictionary.add("Ariège");
+        fakeDictionary.add("Aèrige");
+        fakeDictionary.add("Bokmål");
+        fakeDictionary.add("Gym");
+        fakeDictionary.add("Auk");
+        anagram.dictionary = fakeDictionary;
+
+        List<String> result = anagram.find("Ariège");
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertTrue(result.contains("Aèrige"));
+    }
+
 
 }
